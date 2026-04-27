c9f86ec821a5f23c95f2b6e27415f42be4cd3e7d
Shahd020 <sh.elsharbatly@gmail.com>
2026-04-27 08:35:19 +0300

    need to test

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,12 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react({ fastRefresh: false })],
+  server: {
+    port: 3333,
+    host: '127.0.0.1',
+    headers: {
+      'Cache-Control': 'no-store',
+    },
+  },
 })
